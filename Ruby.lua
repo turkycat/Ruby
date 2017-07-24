90ba389be54782ce2d7447be31e1fd9f78d5343b
Jesse Frush <jesse.frush@gmail.com>
2017-07-24 15:49:48 -0700

    Added Debug function and cleaned up code a bit

--- Ruby.lua
+++ Ruby.lua
@@ -1,46 +1,52 @@
 Ruby = { }
 Ruby.Version = 0.1
 
-function Ruby.PrintDefault( text )
+RUBY_DEBUG = true
+
+function Ruby.Debug( msg )
+    if RUBY_DEBUG then Ruby.Print( msg ) end
+end
+
+function Ruby.Print( text )
     if( DEFAULT_CHAT_FRAME ) then
 	    DEFAULT_CHAT_FRAME:AddMessage("|cffff0000Ruby: |cffffffff"..text);
+    elseif( ChatFrame1 ) then
+        ChatFrame1:AddMessage("|cffff0000Ruby: |cffffffff"..text);
     end
 end
 
-
 function Ruby:OnRubyLoad()
-    --print("Ruby OnRubyLoad called");
-    --self:Print("Ruby OnRubyLoad called");
-    --self:print("Ruby OnRubyLoad called");
+    Ruby.Debug("OnRubyLoad called");
     
-    Ruby.PrintDefault("version "..Ruby.Version.." loaded.");
+    Ruby.Print("version "..Ruby.Version.." loaded.");
 
-	--Ruby:RegisterEvent("PLAYER_ENTERING_WORLD")
-	--Ruby:RegisterEvent("VARIABLES_LOADED")
+	--RubyFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	--RubyFrame:RegisterEvent("VARIABLES_LOADED")
     RubyFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
     
     SLASH_RUBY1 = "/ruby";
     SlashCmdList["RUBY"] = function(msg)
-        Ruby.PrintDefault("you have passed the command \""..msg.."\"");
+        Ruby.Print("you have passed the command \""..msg.."\"");
     end
 
 end
 
-
 function Ruby:OnRubyEvent( event )
-    --Ruby.PrintDefault("OnRubyEvent called");
+    Ruby.Debug("OnRubyEvent called");
     
+    --interestingly, the PLAYER_LEAVE_COMBAT event is not fired when a player runs from combat.
+    --PLAYER_REGEN_ENABLED is fired whenever a character leaves combat for any reason, so we will use this for UI control.
     if event == "PLAYER_REGEN_ENABLED" then
-        --Ruby.PrintDefault("the event was PLAYER_REGEN_ENABLED");
+        Ruby.Debug("the handled event was PLAYER_REGEN_ENABLED");
         
         if not GetItemCount("Mana Ruby") then
-            Ruby.PrintDefault("You are missing a Mana Ruby! Don't forget to conjure another one.");
+            Ruby.Print("You are missing a Mana Ruby! Don't forget to conjure another one.");
         end
         
         if not GetItemCount("Mana Citrine") then
-            Ruby.PrintDefault("You are missing a Mana Citrine! Don't forget to conjure another one.");
+            Ruby.Print("You are missing a Mana Citrine! Don't forget to conjure another one.");
         end
     else
-        --Ruby.PrintDefault("the event was not PLAYER_REGEN_ENABLED");
+        Ruby.Debug("the event "..event.." was not handled.");
     end
 end
